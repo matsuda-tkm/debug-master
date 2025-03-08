e44ce3bbff7b94414c95fde971901c9d32bfe0e9
Moriyuki Suzuki <moriyukisuzuki74@gmail.com>
2025-03-09 03:23:33 +0900

    feat: add CORS middleware to backend and implement client-side data fetching

--- frontend/app/routes/home.tsx
+++ frontend/app/routes/home.tsx
@@ -1,3 +1,4 @@
+import { useEffect, useState } from "react";
 import type { Route } from "./+types/home";
 
 export function meta({}: Route.MetaArgs) {
@@ -7,11 +8,19 @@ export function meta({}: Route.MetaArgs) {
   ];
 }
 
-export default function Home() {
+export const clientLoader = async () => {
+  const response = await fetch('http://localhost:8000');
+  const data = await response.json();
+  return { message: data.message};
+};
+
+export default function Home({ loaderData}: Route.ComponentProps) {
+  const [message, setMessage] = useState<string>(loaderData.message);
+
   return (
     <div>
       <h1>
-        Welcome to Bug Gym !
+        {message}
       </h1>
     </div>
   );
